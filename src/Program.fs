e5f118c9908604c11bd312007008288614510dc1
Alexander C Rees <alexcrees@outlook.com>
2014-02-21 00:20:10 +0000

    Adding some simple error handling and reporting.

--- src/Program.fs
+++ src/Program.fs
@@ -1,11 +1,14 @@
 module Clio.Main
 
 open System
+open System.IO
 open System.Text.RegularExpressions
+
+exception MissingArgument of unit
 
 [<EntryPoint>]
 let main argv =
-    let fail () = printfn "I'm sorry - I couldn't understand that!"
+    let fail () = raise <| InvalidOperationException()
 
     let r = new Regex(@"^-\w.*")
     let (opts, args) = argv |> List.ofArray |> List.partition (r.IsMatch)
@@ -13,25 +16,32 @@ let main argv =
                       | os::_ -> os |> Seq.exists (fun x -> x = o)
                       | [] -> false
 
-    match args with
-    | [] | _::[] -> fail ()
-    | d::o::args ->
-        match o with
-        | "list" ->
-            if hasOption 'p' && args.Length = 0
-            then printfn "I'm sorry, did you mean to add another argument?"
-            else let recursive = hasOption 'r'
-                 let path = if hasOption 'p' then args.[0] else d
-                 Core.show d path recursive
-        | "find" ->
-            let children = hasOption 'c'
-            let recursive = hasOption 'r'
-            match args with
-            | file::[] -> Core.find d file children recursive
+    try
+        match args with
+        | [] | _::[] -> fail ()
+        | d::_ when not <| Directory.Exists d -> raise <| DirectoryNotFoundException()
+        | d::o::args ->
+            match o with
+            | "list" ->
+                if hasOption 'p' && args.Length = 0
+                then raise <| MissingArgument()
+                else let isRecursive = hasOption 'r'
+                     let path = if hasOption 'p' then args.[0] else d
+                     Core.show d path isRecursive
+            | "find" ->
+                let children = hasOption 'c'
+                let isRecursive = hasOption 'r'
+                match args with
+                | file::[] -> Core.find d file children isRecursive
+                | _ -> fail ()
+            | "add" ->
+                match args with 
+                | action::file::[] -> Core.add d action file
+                | _ -> fail ()
             | _ -> fail ()
-        | "add" ->
-            match args with 
-            | action::file::[] -> Core.add d action file
-            | _ -> fail ()
-        | _ -> fail ()
+    with
+        | :? InvalidOperationException -> printfn "I'm sorry - I couldn't understand that!"
+        | :? DirectoryNotFoundException -> printfn "Unfortunately this directory doesn't appear to exist!"
+        | :? MissingArgument -> printfn "I'm sorry, did you mean to add another argument?"
+        | _ -> printfn "Oh, dear! Something really unusual happend."
     0
