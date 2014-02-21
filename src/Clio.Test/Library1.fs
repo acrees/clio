5c35c7f3cdd209b61d51f8020be11e7e3b6912bf
Alexander C Rees <alexcrees@outlook.com>
2014-02-21 01:53:57 +0000

    Setting up unit test project

--- src/Clio.Test/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace Clio.Test
-
-type Class1() = 
-    member this.X = "F#"
---
+++ src/Clio.Test/Utils.fs
@@ -0,0 +1,8 @@
+﻿module Clio.Test.Utils
+
+open Xunit
+open Clio
+
+[<Fact>]
+let ``None is the bind identitiy`` () =
+    None >>= (fun x -> Some 0) |> Option.isNone |> Assert.True
