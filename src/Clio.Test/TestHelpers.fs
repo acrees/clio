defdc137a50fb38d87903055ba0973cc0f545f83
Alexander C Rees <alexcrees@outlook.com>
2014-02-21 08:28:38 +0000

    Testing Utils.

---
+++ src/Clio.Test/TestHelpers.fs
@@ -0,0 +1,9 @@
+﻿[<AutoOpen>]
+module Clio.Test.TestHelpers
+
+open Xunit;
+
+let isSome o = (match o with | None -> false | _ -> true) |> Assert.True
+let isNone o = (match o with | None -> true | _ -> false) |> Assert.True
+
+let is (x:'a) (y:'a) = Assert.Equal<'a>(x, y)
--- src/Clio.Test/Utils.fs
+++ src/Clio.Test/Utils.fs
@@ -5,4 +5,32 @@ open Clio
 
 [<Fact>]
 let ``None is the bind identitiy`` () =
-    None >>= (fun x -> Some 0) |> Option.isNone |> Assert.True
+    None >>= (fun x -> Some 0) |> isNone
+
+[<Fact>]
+let ``Some x bind f applies f to x`` () =
+    Some 4 >>= (fun x -> Some x) |> (fun x -> x.Value) |> is 4
+
+[<Fact>]
+let ``None >>| f does not call f`` () =
+    let value = ref None
+    None >>| (fun x -> value := Some x)
+    !value |> isNone
+
+[<Fact>]
+let ``Some x >>| f calls f with x`` () =
+    let value = ref None
+    Some 3 >>| (fun x -> value := Some x)
+    value.Value.Value |> is 3
+
+[<Fact>]
+let ``Some x is the ifNone identity`` () =
+    let value = ref None
+    ifNone (Some 10) (fun x -> value := Some x) |> (fun x -> x.Value) |> is 10
+    !value |> isNone
+
+[<Fact>]
+let ``None ifNone f calls f then returns None`` () =
+    let value = ref None
+    ifNone None (fun x -> value := Some ()) |> isNone
+    value.Value.Value |> is ()
