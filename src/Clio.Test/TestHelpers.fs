ca1b4deb6bd278a6887626b5004ad9809d636002
Alexander C Rees <me@acrees.co.uk>
2014-02-22 19:20:29 +0000

    Renaming some of the test helpers

--- src/Clio.Test/TestHelpers.fs
+++ src/Clio.Test/TestHelpers.fs
@@ -4,12 +4,12 @@ module Clio.Test.TestHelpers
 open Xunit
 open System
 
-let isSome o = (match o with | None -> false | _ -> true) |> Assert.True
-let isNone o = (match o with | None -> true | _ -> false) |> Assert.True
+let shouldBeSome o = (match o with | None -> false | _ -> true) |> Assert.True
+let shouldBeNone o = (match o with | None -> true | _ -> false) |> Assert.True
 
-let is (x:'a) (y:'a) = Assert.Equal<'a>(x, y)
+let shouldBe (x:'a) (y:'a) = Assert.Equal<'a>(x, y)
 
-let throwsArgumentException f =
+let shouldThrowArgumentException f =
     let mutable called = false
     try f() with | :? ArgumentException -> called <- true
     Assert.True called
--- src/Clio.Test/Utils.fs
+++ src/Clio.Test/Utils.fs
@@ -4,53 +4,53 @@ open Xunit
 open Clio
 
 [<Fact>]
-let ``None is the bind identitiy`` () =
-    None >>= (fun x -> Some 0) |> isNone
+let ``None shouldBe the bind identitiy`` () =
+    None >>= (fun x -> Some 0) |> shouldBeNone
 
 [<Fact>]
 let ``Some x bind f applies f to x`` () =
-    Some 4 >>= (fun x -> Some x) |> (fun x -> x.Value) |> is 4
+    Some 4 >>= (fun x -> Some x) |> (fun x -> x.Value) |> shouldBe 4
 
 [<Fact>]
 let ``None >>| f does not call f`` () =
     let value = ref None
     None >>| (fun x -> value := Some x)
-    !value |> isNone
+    !value |> shouldBeNone
 
 [<Fact>]
 let ``Some x >>| f calls f with x`` () =
     let value = ref None
     Some 3 >>| (fun x -> value := Some x)
-    value.Value.Value |> is 3
+    value.Value.Value |> shouldBe 3
 
 [<Fact>]
-let ``Some x is the ifNone identity`` () =
+let ``Some x shouldBe the ifNone identity`` () =
     let value = ref None
-    ifNone (Some 10) (fun x -> value := Some x) |> (fun x -> x.Value) |> is 10
-    !value |> isNone
+    ifNone (Some 10) (fun x -> value := Some x) |> (fun x -> x.Value) |> shouldBe 10
+    !value |> shouldBeNone
 
 [<Fact>]
 let ``None ifNone f calls f then returns None`` () =
     let value = ref None
-    ifNone None (fun x -> value := Some ()) |> isNone
-    value.Value.Value |> is ()
+    ifNone None (fun x -> value := Some ()) |> shouldBeNone
+    value.Value.Value |> shouldBe ()
 
 [<Fact>]
 let ``GetRelativePath throws when given an empty path`` () =
-    (fun () -> ignore <| getRelativePath "C:/path/" "C:/path/c/" "") |> throwsArgumentException
+    (fun () -> ignore <| getRelativePath "C:/path/" "C:/path/c/" "") |> shouldThrowArgumentException
 
 [<Fact>]
 let ``GetRelativePath throws when given an empty cwd`` () =
-    (fun () -> ignore <| getRelativePath "C:/path/" "" "/x.fs") |> throwsArgumentException
+    (fun () -> ignore <| getRelativePath "C:/path/" "" "/x.fs") |> shouldThrowArgumentException
 
 [<Fact>]
 let ``GetRelativePath returns path if same directory`` () =
-    getRelativePath "C:/src/" "C:/src/" "file.fs" |> is "file.fs"
+    getRelativePath "C:/src/" "C:/src/" "file.fs" |> shouldBe "file.fs"
 
 [<Fact>]
 let ``GetRelativePath returns cwd for .`` () =
-    getRelativePath "C:/src/" "C:/src/x/y/" "." |> is "x/y/"
+    getRelativePath "C:/src/" "C:/src/x/y/" "." |> shouldBe "x/y/"
 
 [<Fact>]
 let ``GetRelativePath returns correct path for unmatched paths`` () =
-    getRelativePath "C:/src/" "C:/src/x/y/" "z/file.fs" |> is "x/y/z/file.fs"
+    getRelativePath "C:/src/" "C:/src/x/y/" "z/file.fs" |> shouldBe "x/y/z/file.fs"
